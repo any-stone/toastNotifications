ad89ddf2cd9bf4d99c8a2b8206e0f9a2cad516d0
any-stone <kuxa.business@gmail.com>
2022-07-21 18:29:13 -0600

    final JS added

--- script.js
+++ script.js
@@ -8,15 +8,30 @@ const messages = [
   "Message Four",
 ];
 
-button.addEventListener("click", () => createNotification());
+const types = ["info", "success", "error"];
 
-function createNotification() {
+button.addEventListener("click", () =>
+  createNotification("This is invalid data", "success")
+);
+
+function createNotification(message = null, type = null) {
   const notif = document.createElement("div");
   notif.classList.add("toast");
+  notif.classList.add(type ? type : getRandomType());
+
+  notif.innerText = message ? message : getRandomMessage();
 
   toasts.appendChild(notif);
+
+  setTimeout(() => {
+    notif.remove();
+  }, 5000);
 }
 
 function getRandomMessage() {
   return messages[Math.floor(Math.random() * messages.length)];
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)];
 }
